a2d4eca981c1a754423313dfded4078b5448ec69
devgiants75 <devgiants75@naver.com>
2023-07-16 17:50:20 +0900

    20230716

--- TypeScript/src/0715/Z_Practice.ts
+++ TypeScript/src/0715/Z_Practice.ts
@@ -50,6 +50,27 @@ console.log(process2('junkuk'));
 // 이 연산자 문자열은 명시적으로 위에서 제시한 4가지 리터럴 중에 하나만을 인자로 받아야 합니다.
 // 또한 함수의 반환 값은 계산 결과인 숫자여야 합니다.
 
+type Operation = "add" | "substract" | "multiply" | "divide";
+
+function calculate(operation: Operation , a: number, b: number):number {
+  switch(operation) {
+    case "add":
+      return a + b;
+    case "substract":
+      return a - b;
+    case "multiply":
+      return a * b;
+    case "divide":
+      return a / b;
+  }
+}
+
+console.log(calculate("add", 4, 2)); // 4 + 2
+console.log(calculate("substract", 4, 2)); // 4 - 2
+console.log(calculate("multiply", 4, 2)); // 4 * 2
+console.log(calculate("divide", 4, 2)); // 4 / 2
+
+
 //! 리터럴 타입을 활용한 예제
 
 // 타입이 left, right, up, down인 Direction 타입 별칭
@@ -58,3 +79,16 @@ console.log(process2('junkuk'));
 
 // 함수 move를 작성하세요
 // 이 함수는 명시적으로 위에서 제시한 4가지 리터럴 중 하나만을 인자로 받아야 합니다.
+
+{
+type Direction = "left" | "right" | "up" | "down";
+
+function move(direction: Direction) {
+  console.log(`Moving ${direction}`)
+}
+
+move("left");
+move("right");
+move("up");
+move("down");
+}
